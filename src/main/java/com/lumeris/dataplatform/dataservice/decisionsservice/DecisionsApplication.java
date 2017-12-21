5682628ebd1e35b5d5abde9c539f694920034333
Sridivya Penmetsa <spenmetsa@lumeris.com>
2017-12-21 10:57:42 -0600

    Added main class

---
+++ src/main/java/com/lumeris/dataplatform/dataservice/decisionsservice/DecisionsApplication.java
@@ -0,0 +1,18 @@
+package com.lumeris.dataplatform.dataservice.decisionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+@Configuration
+@ComponentScan("com.lumeris.dataplatform.dataservice")
+public class DecisionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecisionsApplication.class, args);
+	}	
+}

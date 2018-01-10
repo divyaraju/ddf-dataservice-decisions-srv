645814a2ed52df3b3e957bd3cb44f30a93f0e7d2
Sridivya Penmetsa <spenmetsa@lumeris.com>
2018-01-10 16:04:58 -0600

    removed test

--- src/test/java/com/lumeris/dataplatform/dataservice/decisions/data/service/DecisionsBOImplTest.java
+++ src/test/java/com/lumeris/dataplatform/dataservice/decisions/data/service/DecisionsBOImplTest.java
@@ -1,25 +1,25 @@
-package com.lumeris.dataplatform.dataservice.decisions.data.service;
-
-import static org.junit.Assert.assertNotNull;
-
-import java.util.List;
-
-import org.assertj.core.util.Lists;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import net.minidev.json.parser.ParseException;
-
-@RunWith(SpringRunner.class)
-public class DecisionsBOImplTest {
-
-	@Test
-	public void testPatientRosterBO() throws ParseException, Exception {
-		System.out.println("Executing Patientname");
-		String patient_name = "Debra";
-		List<String> patientSummaryList = Lists.newArrayList();
-		patientSummaryList.add(patient_name);
-		assertNotNull(patientSummaryList);
-	}
-}
+//package com.lumeris.dataplatform.dataservice.decisions.data.service;
+//
+//import static org.junit.Assert.assertNotNull;
+//
+//import java.util.List;
+//
+//import org.assertj.core.util.Lists;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import net.minidev.json.parser.ParseException;
+//
+//@RunWith(SpringRunner.class)
+//public class DecisionsBOImplTest {
+//
+//	@Test
+//	public void testPatientRosterBO() throws ParseException, Exception {
+//		System.out.println("Executing Patientname");
+//		String patient_name = "Debra";
+//		List<String> patientSummaryList = Lists.newArrayList();
+//		patientSummaryList.add(patient_name);
+//		assertNotNull(patientSummaryList);
+//	}
+//}
